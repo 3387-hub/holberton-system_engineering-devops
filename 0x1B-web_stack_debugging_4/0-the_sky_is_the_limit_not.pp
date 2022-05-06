d276cf5c951ff0a321841b8d7c36f87a4e099bab
PabloOsorix <3387@holbertonschool.com>
2022-05-06 09:51:21 -0500

    :pencil2: fix code

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,5 @@
-# Avoid Failed request
-exec { 'failed request':
-command => 'ab -c 100 -n 2000 -l localhost/'
+# Fixing the number of failed requests to 0
+exec { 'repair nginx':
+  command => "sed -i 's/worker_processes 4;/worker_processes 5;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
