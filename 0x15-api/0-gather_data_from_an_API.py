a5874b1de79d8d0b3e6f36de23bea01bcbcf2180
PabloOsorix <3387@holbertonschool.com>
2022-05-19 18:23:33 -0500

    :pencil2: fix pycode

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,24 +4,26 @@ Module that contain a py script that using
 REST API, for a given employee ID, returns
 information about his/her TODO list progress.
 """
-import requests
-import json
 from sys import argv
+import requests
 
+if __name__ == "__main__":
 
-cookies = {"sessioncookie": "1233456789"}
-user_r = requests.get(f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
-                      ).json()
-all_r = requests.get("https://jsonplaceholder.typicode.com/todos").json()
+    cookies = {"sessioncookie": "1233456789"}
+    user_r = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{argv[1]}").json()
 
-counter = 0
-num_task = 0
-tasks = []
+    all_r = requests.get("https://jsonplaceholder.typicode.com/todos").json()
 
-for i in all_r:
-    if i["userId"] == user_r["id"]:
-        tasks.append("\t" + i["title"])
-        counter += 1
-    num_task += 1
-print(f"Employee {user_r['name']} id done with task({counter}/{num_task}):")
-print('\n'.join(tasks))
+    counter = 0
+    num_task = 0
+    tasks = []
+
+    for i in all_r:
+        if i["userId"] == user_r["id"]:
+            tasks.append("\t" + i["title"])
+            counter += 1
+        num_task += 1
+    print(f"Employee {user_r['name']} id done with task\
+        ({counter}/{num_task}):")
+    print('\n'.join(tasks))
