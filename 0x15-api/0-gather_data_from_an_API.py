a50b1ee043b3da14ab81ac0eb2688e12b8eb5f30
PabloOsorix <3387@holbertonschool.com>
2022-05-19 18:19:51 -0500

    :heavy_plus_sign: script that find the number of task done by a user

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module that contain a py script that using
+REST API, for a given employee ID, returns
+information about his/her TODO list progress.
+"""
+import requests
+import json
+from sys import argv
+
+
+cookies = {"sessioncookie": "1233456789"}
+user_r = requests.get(f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
+                      ).json()
+all_r = requests.get("https://jsonplaceholder.typicode.com/todos").json()
+
+counter = 0
+num_task = 0
+tasks = []
+
+for i in all_r:
+    if i["userId"] == user_r["id"]:
+        tasks.append("\t" + i["title"])
+        counter += 1
+    num_task += 1
+print(f"Employee {user_r['name']} id done with task({counter}/{num_task}):")
+print('\n'.join(tasks))
