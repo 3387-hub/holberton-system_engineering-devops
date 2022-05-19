ac8a9bdaa63198d5baa69ab2f444128280c47cf9
PabloOsorix <3387@holbertonschool.com>
2022-05-19 18:52:43 -0500

    :pencil2: fix format and fix the exercise

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Module that contain a py script that using
+REST API, for a given employee ID, returns
+information about his/her /todos list progress.
+"""
 import requests
 from sys import argv
 
@@ -6,20 +11,21 @@ from sys import argv
 if __name__ == "__main__":
 
     cookies = {"sessioncookie": "1233456789"}
-    user_r = requests.get(
-        f"https://jsonplaceholder.typicode.com/users/{argv[1]}").json()
+    user_r = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                          .format(argv[1])).json()
 
     all_r = requests.get("https://jsonplaceholder.typicode.com/todos").json()
 
-    counter = 0
-    num_task = 0
+    total_task = 0
+    done_task = 0
     tasks = []
-
+    
     for i in all_r:
         if i["userId"] == user_r["id"]:
-            tasks.append("\t" + i["title"])
-            counter += 1
-        num_task += 1
-    print(f"Employee {user_r['name']} id done with task\
-        ({counter}/{num_task}):")
+            total_task += 1
+            if i["completed"] is True:
+                tasks.append("\t" + i["title"])
+                done_task += 1
+    print("Employee {} id done with task({}/{}):"
+          .format(user_r['name'], done_task, total_task))
     print('\n'.join(tasks))
